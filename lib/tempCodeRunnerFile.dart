3771be66cd7fe6cb89ab272df5c3a538cb0a17f4
SeymaSakoglu <148337050+SeymaSakoglu@users.noreply.github.com>
2025-09-04 16:51:35 +0300

    - Gunluk hedef degistirme butonu eklendi - Su icme hatirleticisi eklendi

--- lib/tempCodeRunnerFile.dart
+++ lib/tempCodeRunnerFile.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
@@ -26,32 +27,12 @@ class SuTakipHomePage extends StatefulWidget {
 
 class _SuTakipHomePageState extends State<SuTakipHomePage> {
   int icililenMiktar = 0;
-  late int gunlukHedef = 2000;
+  final int gunlukHedef = 2000;
   bool mesajGoster = false;
-  DateTime bugun = DateTime.now();
 
   void suEkle(int miktar) {
     setState(() {
       icililenMiktar += miktar;
-
-      if (icililenMiktar < gunlukHedef && icililenMiktar % 500 == 0) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text("Su içmeyi unutma 💧"),
-            duration: Duration(seconds: 3),
-          ),
-        );
-      }
-
-      if (icililenMiktar >= gunlukHedef) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text("Tebrikler! Günlük Hedefine Ulaştın 🎉 "),
-            backgroundColor: Colors.lightGreen,
-            duration: Duration(seconds: 3),
-          ),
-        );
-      }
     });
   }
 
@@ -61,58 +42,19 @@ class _SuTakipHomePageState extends State<SuTakipHomePage> {
     });
   }
 
-  void gunlukHedefDegistir() {
-    showDialog(
-      context: context,
-      builder: (context) {
-        TextEditingController controller = TextEditingController(
-          text: gunlukHedef.toString(),
-        );
-        return AlertDialog(
-          title: Text("Günlük Hedefi Belirle :"),
-          content: TextField(
-            controller: controller,
-            keyboardType: TextInputType.number,
-            decoration: InputDecoration(hintText: 'Örn 2400'),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('İptal'),
-            ),
-            ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  gunlukHedef = int.tryParse(controller.text) ?? gunlukHedef;
-                });
-                Navigator.pop(context);
-              },
-              child: Text('Kaydet'),
-            ),
-          ],
-        );
-      },
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     int kalanSu =
         gunlukHedef - icililenMiktar > 0 ? gunlukHedef - icililenMiktar : 0;
 
     double ilerlemeDurumu = icililenMiktar / gunlukHedef;
-    if (ilerlemeDurumu > 1.0) ilerlemeDurumu = 1.0;
+    if (ilerlemeDurumu > 1.0) ilerlemeDurumu = 1.0; // %100 geçmesin diye
 
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.lightBlue,
         title: Text("Su Takip Uygulaması"),
         centerTitle: true,
-        actions: [
-          IconButton(icon: Icon(Icons.edit), onPressed: gunlukHedefDegistir),
-        ],
       ),
       backgroundColor: const Color(0xFFE0F0FF),
       body: Stack(
@@ -172,10 +114,8 @@ class _SuTakipHomePageState extends State<SuTakipHomePage> {
                 ),
                 const SizedBox(height: 100),
 
-                // Butonlar - Row ile yan yana
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.end,
+                Wrap(
+                  spacing: 15,
                   children: [
                     SuButonu(miktar: 200, tiklandiginda: () => suEkle(200)),
                     SuButonu(miktar: 300, tiklandiginda: () => suEkle(300)),
@@ -221,56 +161,87 @@ class SuButonu extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: tiklandiginda,
-      child: SizedBox(
-        width: 100,
-        height: 160,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            // Görsel
-            _buildImage(),
-            const SizedBox(height: 8),
+      child: Column(
+        children: [
+          Row(
+            children: [
+            Column(
+              children: [
+              // Bardak görseli
+            if (miktar == 200)
+              Image.asset(
+                'assets/images/glass.png',
+                width: 80,
+                height: 80,
+                fit: BoxFit.contain),
+            const SizedBox(height: 6),
+          
             // Miktar yazısı
             Text(
               "$miktar ml",
+              textAlign: TextAlign.center,
               style: const TextStyle(
                 color: Colors.blue,
                 fontWeight: FontWeight.bold,
                 fontSize: 16,
               ),
             ),
-            const SizedBox(height: 8),
-          ],
-        ),
+            ],
+            ),
+          
+            Column(
+              children: [
+              // Bardak görseli
+            if (miktar == 300)
+              Image.asset(
+                'assets/images/glass.png',
+                width: 80,
+                height: 100,
+                fit: BoxFit.contain),
+            const SizedBox(height: 6),
+          
+            // Miktar yazısı
+            Text(
+              "$miktar ml",
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            ],
+            ),
+          
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+              // Bardak görseli
+            if (miktar == 500)
+              Image.asset(
+                'assets/images/bottle.png',
+                width: 100,
+                height: 120,
+                fit: BoxFit.contain),
+            const SizedBox(height: 6),
+          
+            // Miktar yazısı
+            Text(
+              "$miktar ml",
+              textAlign: TextAlign.center,
+              style: const TextStyle(
+                color: Colors.blue,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
+            ),
+            ],
+            ),
+            ],
+          ),
+        ]
+
       ),
     );
-  }
-
-  Widget _buildImage() {
-    switch (miktar) {
-      case 200:
-        return Image.asset(
-          'assets/images/glass.png',
-          width: 75,
-          height: 100,
-          fit: BoxFit.contain,
-        );
-      case 300:
-        return Image.asset(
-          'assets/images/glass.png',
-          width: 80,
-          height: 100,
-          fit: BoxFit.contain,
-        );
-      case 500:
-        return Image.asset(
-          'assets/images/bottle.png',
-          width: 100,
-          height: 120,
-          fit: BoxFit.contain,
-        );
-      default:
-        return Container(width: 80, height: 100, color: Colors.grey);
-    }
   }
 }
